fe13240427ba87b0ff1acb1bcb07fdebb957ddab
Tonto <823498927@qq.com>
2019-05-09 14:03:17 +0800

    fix 同步framework

--- src/main/java/com/paladin/Application.java
+++ src/main/java/com/paladin/Application.java
@@ -2,7 +2,6 @@ package com.paladin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableTransactionManagement
 @SpringBootApplication
 @EnableScheduling
-@EnableConfigurationProperties
 public class Application {
 	
 	public static void main(String[] args) {
